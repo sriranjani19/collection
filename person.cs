b4653ddbfe30a054b628c968152895d564aae352
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-22 19:44:39 +0530

    Add files via upload

---
+++ person.cs
@@ -0,0 +1,33 @@
+﻿
+
+   
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    public class Person
+    {
+        public string PhoneNo;
+        public string Name;
+        public string Address;
+        public string City;
+        public string State;
+        public string Zip;
+        public string EmailId;
+
+        public Person(string phoneNo, string name, string address, string city, string state, string zip, string emailid)
+        {
+            this.PhoneNo = phoneNo;
+            this.Name = name;
+            this.Address = address;
+            this.City = city;
+            this.State = state;
+            this.Zip = zip;
+            this.EmailId = emailid;
+        }
+    }
+}
